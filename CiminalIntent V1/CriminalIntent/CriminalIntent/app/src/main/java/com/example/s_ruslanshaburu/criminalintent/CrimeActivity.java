1298f07ac306c5e8a4838ebd7c0ea9b37d322673
Ruslan273 <usruss@live.com>
2015-02-27 05:41:02 -0800

    V2

---
+++ CiminalIntent V1/CriminalIntent/CriminalIntent/app/src/main/java/com/example/s_ruslanshaburu/criminalintent/CrimeActivity.java
@@ -0,0 +1,57 @@
+package com.example.s_ruslanshaburu.criminalintent;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.UUID;
+
+
+public class CrimeActivity extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment createFragment(){
+
+        UUID crimeId = (UUID)getIntent()
+                .getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+
+        return CrimeFragment.newInstance(crimeId);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_crime, container, false);
+            return rootView;
+        }
+    }
+}
